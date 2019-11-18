ba6a2331ae988e2e40ae7736867a42657f97bd78
one_mike <moaduamah@gmail.com>
2019-11-18 12:47:51 +0000

    filename changes

---
+++ Mail Suite/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Mail_Suite
+{
+    public class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+
+        {
+            
+            Application.Run(new Form1());
+
+          
+        }
+
+    }
+
+}
